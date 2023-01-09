35dbb6dca888af438db8e7dcd8ec43d05780086d
Andrei H <84508308+AndryCake@users.noreply.github.com>
2023-01-09 16:44:14 +0200

    Added browser selection GUI (unused)
    This is a GUI that allows the user to select the browser(s) they wish to install (see https://github.com/Ultra980/danora-linux/issues/4). This GUI is currently unused and will be later merged into danora-firstboot.

---
+++ browsersel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+zenity --list \
+--title=Browser \
+--column " " --column "Browser" --column "Package Name" --column "Install Command" --column "Install Method/Repository" --separator " & " --checklist --print-column=4 --width=545 --height=300 --text "Please select the browser(s) you want to install from the list below:" --hide-column=4 \
+"TRUE" "Firefox" "org.mozilla.firefox" "flatpak install org.mozilla.firefox" "Flatpak (flathub)" \
+"FALSE" "Vivaldi" "vivaldi" "wget https://vivaldi.com/download/ | rpm-ostree install" "rpm-ostree" \
+"FALSE" "Google Chromium" "org.chromium.Chromium" "flatpak install org.chromium.Chromium" "Flatpak (flathub)" \
+"FALSE" "Ungoogled Chromium" "com.github.Eloston.UngoogledChromium" "flatpak install com.github.Eloston.UngoogledChromium" "Flatpak (flathub)"
