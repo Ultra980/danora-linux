cef1dd9a1dc0456090dae1de5d15df8e7512262f
Andrei H <84508308+AndryCake@users.noreply.github.com>
2023-01-15 10:02:30 +0000

    Fixed browser selection GUI (browsersel.sh)
    Added "Install Method/Repository" field to Waterfox, fixed package name for Waterfox (was "waterfox", now "N/A"). Changed default of Firefox to FALSE (was TRUE)

--- browsersel.sh
+++ browsersel.sh
@@ -3,8 +3,8 @@
 zenity --list \
 --title=Browser \
 --column " " --column "Browser" --column "Package Name" --column "Install Command" --column "Install Method/Repository" --separator " & " --checklist --print-column=4 --width=545 --height=300 --text "Please select the browser(s) you want to install from the list below:" --hide-column=4 \
-"TRUE" "Waterfox (Reccomended)" "waterfox" "/usr/bin/install_waterfox" \
-"TRUE" "Firefox" "org.mozilla.firefox" "flatpak install org.mozilla.firefox" "Flatpak (flathub)" \
+"TRUE" "Waterfox (Reccomended)" "N/A" "/usr/bin/install_waterfox" "Custom install script" \
+"FALSE" "Firefox" "org.mozilla.firefox" "flatpak install org.mozilla.firefox" "Flatpak (flathub)" \
 "FALSE" "Vivaldi" "vivaldi" "wget https://vivaldi.com/download/ | rpm-ostree install" "rpm-ostree" \
 "FALSE" "Google Chromium" "org.chromium.Chromium" "flatpak install org.chromium.Chromium" "Flatpak (flathub)" \
 "FALSE" "Ungoogled Chromium" "com.github.Eloston.UngoogledChromium" "flatpak install com.github.Eloston.UngoogledChromium" "Flatpak (flathub)"
